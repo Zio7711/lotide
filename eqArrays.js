c81969127063c86b4f0a249011853c79f0838226
Zio <zi.ao.tan@outlook.com>
2021-04-28 00:13:48 +0000

    updated formatting template

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,19 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if(array2.length != array1.length){
-    return false
+  if (array2.length !== array1.length) {
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]){
-      return false
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
