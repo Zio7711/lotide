906bfe3e9355e7ea530e4a6dc93c49970bd9640b
Zio <zi.ao.tan@outlook.com>
2021-05-03 21:07:55 +0000

    refactored with mocha and chai method

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,30 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail.js');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+describe('#tail testing', () => {
+  it("original array ['Yo Yo', 'Lighthouse', 'Labs'], length should be 2", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    let result = tail(words); 
+    assert.equal(result.length, 2); 
+  });
 
-let result = tail(words); 
-assertEqual(result.length, 2); 
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+  it("original array ['Yo Yo', 'Lighthouse', 'Labs'], index 0 should be 'Lighthouse'", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    let result = tail(words); 
+    assert.equal(result[0], "Lighthouse");
+  });
+
+  it("original array ['Yo Yo', 'Lighthouse', 'Labs'], index 1 should be 'Labs'", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    let result = tail(words); 
+    assert.equal(result[1], "Labs");
+  });
+});
+
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// let result = tail(words); 
+// assertEqual(result.length, 2); 
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
