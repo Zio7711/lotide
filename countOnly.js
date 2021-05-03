b757d95009a74c71e82587a40d0bcf6752ac98e2
Zio <zi.ao.tan@outlook.com>
2021-05-03 12:25:43 -0700

    updated formatting

--- countOnly.js
+++ countOnly.js
@@ -5,13 +5,11 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌Assertion Passed: ${actual} !== ${expected}`);
   }
 };
-
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
+  
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
