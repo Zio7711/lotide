5a0d9ff4d47b0ebac7825b3d43d0e15fe3b3ee7e
Zio <zi.ao.tan@outlook.com>
2021-05-03 20:42:51 +0000

    finished refactoring asserArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,15 @@
-const eqArrays = function(array1, array2) {
-  if (array2.length !== array1.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(array3, array4) {
   if (eqArrays(array3, array4)) {
-    console.log(`✅✅✅ ${array3} === ${array4}`);
+    console.log(`✅✅✅ Passed: ${array3} === ${array4}`);
   } else {
-    console.log(`❌❌❌ ${array3} !== ${array4}`);
+    console.log(`❌❌❌ Failed: ${array3} !== ${array4}`);
   }
   return;
 };
 
-assertArraysEqual([1, 2, 3], [3 ,2 ,1]);
-assertArraysEqual([1, 2, 3], [1 ,2 ,3]);
+// assertArraysEqual([1, 2, 3], [3 ,2 ,1]);
+// assertArraysEqual([1, 2, 3], [1 ,2 ,3]);
+
+module.exports = assertArraysEqual;
