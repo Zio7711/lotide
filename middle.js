d307a4eb4685103f31fe6717be65edbf942f759e
Zio <zi.ao.tan@outlook.com>
2021-05-03 20:48:27 +0000

    refactored middle and created middleTest.js file

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array2.length !== array1.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Passed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays.js');
+const assertEqual = require('./assertEqual');
 
 const middle = function(array) {
   if (array.length <= 2) {
@@ -30,9 +13,11 @@ const middle = function(array) {
   };
 };
 
-assertEqual(eqArrays(middle([1]), []), true)
-assertEqual(eqArrays(middle([1, 2]), []), true)
-assertEqual(eqArrays(middle([1, 2, 3]), [2]), true)
-assertEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true)
-assertEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true)
-assertEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true)
+// assertEqual(eqArrays(middle([1]), []), true);
+// assertEqual(eqArrays(middle([1, 2]), []), true);
+// assertEqual(eqArrays(middle([1, 2, 3]), [2]), true);
+// assertEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+// assertEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+// assertEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+
+module.exports = middle;
